7ff9fe7076be36ff66ed811f413e5361002b2b50
Marat Bunyatov <marat@Marats-MBP.home>
2018-04-12 00:39:04 -0400

    roles

--- assets/js/card-templates.js
+++ assets/js/card-templates.js
@@ -40,6 +40,45 @@ tpl.positionCard = _.template(`
   </div>
 `);
 
+tpl.tradeCard = _.template(`
+  <div class="trade-card card mb-4 box-shadow col-xl-3">
+    <div class="card-header">
+      <h4 class="my-0 font-weight-normal"><%= name %></h4>
+      <h6 class="mt-1 my-0 font-weight-normal">9088890</h6>
+    </div>
+    <div class="card-body">
+      <table class="metrics my-2">
+        <tr>
+          <td class="metrics-name">Side:</td>
+          <td class="metrics-value"><%= side %></td>
+        </tr>
+        <tr>
+          <td class="metrics-name">Amount On Loan:</td>
+          <td class="metrics-value"><%= amountLoaned %></td>
+        </tr>
+        <tr>
+          <td class="metrics-name">Interest Earned:</td>
+          <td class="metrics-value"><%= interestEarned %></td>
+        </tr>
+      </table>
+      <table class="metrics my-2">
+        <tr>
+          <td class="metrics-name">Open Inventory:</td>
+          <td class="metrics-value"><%= openInventory %></td>
+        </tr>
+        <tr>
+          <td class="metrics-name">Duration Open:</td>
+          <td class="metrics-value"><%= openDuration %></td>
+        </tr>
+      </table>
+      <form class="buy2cover">
+        <p><input type="text" name="buy2cover" value="" class="form-control" /></p>
+        <p><input type="submit" value="Buy To Cover" class="btn btn-primary" /></p>
+      </form>
+    </div>
+  </div>
+`);
+
 tpl.inventoryCard = _.template(`
   <div class="inventory-card card bg-dark text-white mb-4 box-shadow col-xl-3">
     <div class="card-header">
