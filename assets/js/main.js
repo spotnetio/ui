06722486e5d8c146b2c1d7c0e71e0f61d75ff674
Marat Bunyatov <marat@Marats-MBP.home>
2018-04-12 00:39:04 -0400

    roles

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,14 +37,45 @@ const Positions = (function() {
           url: MATCHER_URL + '/inventory/'+App.account,
           success: function( data ) {
             for(let token in data) {
-              result.push({
-                name: App.contractsByAddress[token].name,
-                side: 'LEND',
-                amountLoaned: '8000 BNB',
-                interestEarned: '75.83 ETH',
-                openInventory: data[token],
-                openDuration: '6 Days',
-              });
+              if(data[token]['lender'] > 0) {
+                result.push({
+                  name: App.contractsByAddress[token].name,
+                  side: 'LEND',
+                  amountLoaned: '8000 BNB',
+                  interestEarned: '75.83 ETH',
+                  openInventory: data[token]['lender'],
+                  openDuration: '1 Day(s)',
+                });
+              }
+            }
+            resolve(result);
+          },
+        });
+      });
+    },
+  };
+})();
+
+const Trades = (function() {
+  return {
+    get() {
+      return new Promise((resolve, reject) => {
+        let result = [];
+        console.log(App.account)
+        $.ajax({
+          url: MATCHER_URL + '/inventory/'+App.account,
+          success: function( data ) {
+            for(let token in data) {
+              if(data[token]['trader'] > 0) {
+                result.push({
+                  name: App.contractsByAddress[token].name,
+                  side: 'TRADE',
+                  amountLoaned: '8000 BNB',
+                  interestEarned: '75.83 ETH',
+                  openInventory: data[token]['trader'],
+                  openDuration: '1 Day(s)',
+                });
+              }
             }
             resolve(result);
           },
@@ -57,13 +88,68 @@ const Positions = (function() {
 const DisplayCards = function($, _) {
   $('.inventory').empty();
   $('.positions').empty();
+  $('.trades').empty();
   Inventory.get().then((inventoryCards) => {
     _.forEach(inventoryCards, card => $('.inventory').append(tpl.inventoryCard(card)));
   });
   Positions.get().then((positionCards) => {
     _.forEach(positionCards, card => $('.positions').append(tpl.positionCard(card)));
   });
+  Trades.get().then((tradeCards) => {
+    _.forEach(tradeCards, card => $('.trades').append(tpl.tradeCard(card)));
+  });
+};
 
+(function($, _) {
+  $('.inventory.card-deck').on('click', 'form.trade button.lend', (event) => {
+    event.preventDefault();
+    let formTrade = $(event.target).parents('form.trade');
+    let $lend = formTrade.find('input[name="trade"]');
+    let lendAmt = parseInt($lend.val());
+    console.log('lendAmt ' + lendAmt);
+    let $tokenAddress = formTrade.find('input[name="tokenAddress"]');
+    let tokenAddress = $tokenAddress.val();
+    if (isNaN(lendAmt)) {
+      console.log('lend amount is not a number');
+      $lend.val('');
+      return;
+    }
+    $.ajax({
+      type: "POST",
+      url: MATCHER_URL + '/inventory/'+App.account,
+      data: { 
+        token: tokenAddress, 
+        amount: lendAmt, 
+        role: 'lender' },
+    }).always(function(data) {
+        DisplayCards($, _);
+    });
+    $lend.val('');
+  });
+  $('.inventory.card-deck').on('click', 'form.trade button.short', (event) => {
+    event.preventDefault();
+    let formTrade = $(event.target).parents('form.trade');
+    let $trade = formTrade.find('input[name="trade"]');
+    let shortAmt = parseInt($trade.val());
+    let $tokenAddress = formTrade.find('input[name="tokenAddress"]');
+    let tokenAddress = $tokenAddress.val();
+    if (isNaN(shortAmt)) {
+      console.log('short is not a number');
+      $trade.val('');
+      return;
+    }
+    $.ajax({
+      type: "POST",
+      url: MATCHER_URL + '/inventory/' + App.account,
+      data: { 
+        token: tokenAddress, 
+        amount: shortAmt, 
+        role: 'trader' },
+    }).always(function(data) {
+        DisplayCards($, _);
+    });
+    $trade.val('');
+  });
   $('.positions.card-deck').on('submit', 'form.recall', (event) => {
     event.preventDefault();
     let $recall = $(event.target).find('input[name="recall"]');
@@ -76,38 +162,16 @@ const DisplayCards = function($, _) {
     console.log('recalling', recallAmt);
     $recall.val('');
   });
-  $('.inventory.card-deck').on('click', 'form.trade button.lend', (event) => {
+  $('.trades.card-deck').on('submit', 'form.buy2cover', (event) => {
     event.preventDefault();
-    let formTrade = $(event.target).parents('form.trade');
-    let $trade = formTrade.find('input[name="trade"]');
-    let lendAmt = parseInt($trade.val());
-    let $tokenAddress = formTrade.find('input[name="tokenAddress"]');
-    let tokenAddress = $tokenAddress.val();
-    if (isNaN(lendAmt)) {
-      console.log('lend amount is not a number');
-      $trade.val('');
+    let $buy2cover = $(event.target).find('input[name="buy2cover"]');
+    let buy2coverAmt = parseInt($buy2cover.val());
+    if (isNaN(buy2coverAmt)) {
+      console.log('buy2cover is not a number');
+      $buy2cover.val('');
       return;
     }
-    $.ajax({
-      type: "POST",
-      url: MATCHER_URL + '/inventory/'+App.account,
-      data: { token: tokenAddress, amount: lendAmt },
-    }).always(function(data) {
-        console.log('Post request completed ' + JSON.stringify(data));
-        DisplayCards($, _);
-    });
-    $trade.val('');
+    console.log('buying back', buy2coverAmt);
+    $buy2cover.val('');
   });
-  $('.inventory.card-deck').on('click', 'form.trade button.short', (event) => {
-    event.preventDefault();
-    let $trade = $(event.target).parents('form.trade').find('input[name="trade"]');
-    let shortAmt = parseInt($trade.val());
-    if (isNaN(shortAmt)) {
-      console.log('short is not a number');
-      $trade.val('');
-      return;
-    }
-    console.log('short', shortAmt);
-    $trade.val('');
-  });
-};
+})(jQuery, _);
